4971e8558fb1e1ab3ea1a9a048a3d993ee0c921e
Prince Mishra <mrprimi09@gmail.com>
2024-08-01 22:30:49 +0530

    Update weather.js

--- weather.js
+++ weather.js
@@ -12,24 +12,24 @@ function updateWeather(data) {
     document.querySelector('.wi').innerHTML = data.current.wind_kph + "kph"
 
     if (data.current.condition.text == "Mist") {
-        imgElement.src = "assets/images/mist.png";
+        imgElement.src = "mist.png";
 
     } else if (data.current.condition.text == "Clear") {
-        imgElement.src = "assets/images/clear.png";
+        imgElement.src = "clear.png";
 
     } else if (data.current.condition.text == "Partly cloudy") {
-        imgElement.src = "assets/images/clouds.png";
+        imgElement.src = "clouds.png";
 
     } else if (data.current.condition.text == "Drizzle") {
-        imgElement.src = "assets/images/drizzle.png";
+        imgElement.src = "drizzle.png";
     } else if (data.current.condition.text == "Rain") {
-        imgElement.src = "assets/images/rain.png";
+        imgElement.src = "rain.png";
     }
     else if (data.current.condition.text == "Snow") {
-        imgElement.src = "assets/images/snow.png";
+        imgElement.src = "snow.png";
     }
     else{
-        imgElement.src = "assets/images/clouds.png";
+        imgElement.src = "clouds.png";
     }
     input = "";
 }
